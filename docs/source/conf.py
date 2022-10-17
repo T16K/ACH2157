9c7e02700d77cef69ba5c624203635d0cb2d1a3e
t1k <gt1ariga@gmail.com>
2022-10-17 10:49:36 -0300

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ACH2157'
+copyright = '2022, CFA'
+author = 'Tsuyoshi'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.2.0'
 
 # -- General configuration
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -9,6 +9,8 @@ and offers a *simple* and *intuitive* API.
 Check out the :doc:`usage` section for further information, including
 how to :ref:`installation` the project.
 
+Testando file index.rst 1 2 3 aaa.
+
 .. note::
 
    This project is under active development.
